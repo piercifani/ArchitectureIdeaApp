4faa9fe1a50b0b65ab737385b4f436e5f8b854f1
Pierluigi Cifani <pcifani@theleftbit.com>
2024-05-20 11:30:20 +0200

    Adds @State at the App level like Apple suggests
    https://developer.apple.com/documentation/swiftui/migrating-from-the-observable-object-protocol-to-the-observable-macro/#Migrate-other-source-code

--- ArchitectureIdea/ArchitectureIdeaApp.swift
+++ ArchitectureIdea/ArchitectureIdeaApp.swift
@@ -3,14 +3,20 @@ import SwiftUI
 @main
 struct ArchitectureIdeaApp: App {
     
-    private let recordingState = RecordingState()
-    
-    private var recordingController: RecordingController
-    private var contentViewModel: ContentViewModel?
+    @State
+    private var recordingState: RecordingState
+
+    @State
+    private var contentViewModel: ContentViewModel
     
     init() {
-        self.recordingController = RecordingController(recordingState: recordingState)
-        self.contentViewModel = ContentViewModel(recordingState: recordingState, recordingController: recordingController)
+        let recordingState = RecordingState()
+        self.recordingState = recordingState
+        let recordingController = RecordingController(recordingState: recordingState)
+        self.contentViewModel = ContentViewModel(
+            recordingState: recordingState,
+            recordingController: recordingController
+        )
     }
     
     var body: some Scene {
